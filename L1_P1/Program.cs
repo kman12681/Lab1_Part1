eff64ab4f1df43513e23e34962c73c208eeb6bb4
kman12681 <keith.rigling@gmail.com>
2018-04-10 08:46:35 -0400

    basic working

--- L1_P1/Program.cs
+++ L1_P1/Program.cs
@@ -1,30 +1,59 @@
 ﻿using System;
 
-namespace L1_P1
+namespace SimpleSum
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x, y;
+        First:
+            Console.WriteLine("Enter a positive three digit number: ");
+            int num1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter a number without decimals");
-            x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The number you just wrote contains this many digits: {0} ", GetDigits(x, 0));
-            Console.ReadLine();
+            if (num1 > 999 || num1 < 000)
+            {
+                Console.WriteLine("This is not a positive three digit number");
+                goto First;
+            }
 
-            Console.WriteLine("Now, please write another number (without decimals) with the same number of digits as the first number");
-            y = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The number you just wrote contains this many digits: {0} ", GetDigits(y, 0));
-            Console.ReadLine();
+        Second:
+            Console.WriteLine("Thank you, now enter another positive three digit number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num2 > 999 || num2 < 000)
+            {
+                Console.WriteLine("This is not a positive three digit number");
+                goto Second;
+            }
+
+            int total = Sum(num1, num2);
+
+            // exceptions to handle, when decimals are used, when numbers larger than ints are used
+            // how to move the if statement to a second method
+
+            if (total == 000 ||
+                total == 111 ||
+                total == 222 ||
+                total == 333 ||
+                total == 444 ||
+                total == 555 ||
+                total == 666 ||
+                total == 777 ||
+                total == 888 ||
+                total == 999)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.Read();
         }
-
-        private static int GetDigits(int n1, int nodigits)
+        public static int Sum(int x, int y)
         {
-            if (n1 == 0)
-                return nodigits;
-
-            return GetDigits(n1 / 10, ++nodigits);
+            int total = x + y;
+            return total;
         }
+
     }
 }
