61e5026284ef18a46542e14b9accafc37bfadc9c
kman12681 <keith.rigling@gmail.com>
2018-04-09 20:28:52 -0400

    first commit

--- L1_P1/Program.cs
+++ L1_P1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L1_P1
 {
@@ -10,6 +6,25 @@ namespace L1_P1
     {
         static void Main(string[] args)
         {
+            int x, y;
+
+            Console.WriteLine("Please enter a number without decimals");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number you just wrote contains this many digits: {0} ", GetDigits(x, 0));
+            Console.ReadLine();
+
+            Console.WriteLine("Now, please write another number (without decimals) with the same number of digits as the first number");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number you just wrote contains this many digits: {0} ", GetDigits(y, 0));
+            Console.ReadLine();
+        }
+
+        private static int GetDigits(int n1, int nodigits)
+        {
+            if (n1 == 0)
+                return nodigits;
+
+            return GetDigits(n1 / 10, ++nodigits);
         }
     }
 }
