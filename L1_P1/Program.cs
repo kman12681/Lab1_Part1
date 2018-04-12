2c14d63a6de0f7f7270111a429091a819e73a683
kman12681 <keith.rigling@gmail.com>
2018-04-12 18:53:27 -0400

    bool and task method

--- L1_P1/Program.cs
+++ L1_P1/Program.cs
@@ -1,133 +1,132 @@
 ﻿using System;
-//using System.Linq;
 
 namespace L1_P1
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            // Number of digits requested from user
+                // Welcome User
 
-            string digits = "four";
-            int digitsCount = 4;
+                Console.WriteLine();
+                Console.WriteLine("Hello, this program will assess whether the individual digits in two numbers all add to the same value.");
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey(true);
 
-            // Welcome User
+                Console.WriteLine();
+                Console.WriteLine("The console will read 'True' if the digits add to the same value, and 'False' if they do not.");
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey(true);
 
-            Console.WriteLine();
-            Console.WriteLine("Hello, this program will assess whether the individual digits in two numbers all add to the same value.");
-            Console.WriteLine();
-            Console.WriteLine("Press any key to continue.");
-            Console.ReadKey(true);
+                // First Input
 
-            Console.WriteLine();
-            Console.WriteLine("The console will read 'True' if the digits add to the same value, and 'False' if they do not.");
-            Console.WriteLine();
-            Console.WriteLine("Press any key to continue.");
-            Console.ReadKey(true);
+                string num1;
 
-            // First Input
+                while (true)
+                {
+                    while (true) // program won't continue until three digits entered
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Please enter a three digit number:");
+                        Console.WriteLine();
+                        num1 = Console.ReadLine();
+                        int firstNumber;
+                        bool num1Bool = Int32.TryParse(num1, out firstNumber);
 
-            First:
-            Console.WriteLine();
-            Console.WriteLine("Please enter a " + digits + " digit number:");
-            Console.WriteLine();
-            string num1 = Console.ReadLine();
-            int len1 = num1.Length;
+                        // Input Validation
 
-            // Input Validation
+                        if (num1.Length == 3 && num1Bool)
+                            break;
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Not a three digit number.");
+                        }
+                    }
 
-            if (len1 != digitsCount)
+                    // Second Input 
+
+                    string num2;
+
+                    while (true) // won't continue until three digits entered
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Enter another three digit number.");
+                        Console.WriteLine();
+                        num2 = Console.ReadLine();
+                        int secondNumber;
+                        bool num2Bool = Int32.TryParse(num2, out secondNumber);
+
+                        // Input Validation
+
+                        if (num2.Length == 3 && num2Bool)
+                            break;
+
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Not three digits.");
+                        }
+                    }
+                    
+                    // calling Task method
+
+                    Task(num1, num2);
+                
+                    while (true) // will run program again if user enters "Y"
+
+                    {
+
+                        Console.WriteLine("Enter Y to try again");
+                        string input = Console.ReadLine().ToUpper();
+
+                    if (input == "Y")
+                        break;
+                    
+                    else
+
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Not a valid entry.");
+                        Console.WriteLine();
+                    }
+
+                }
+
+            }
+        }
+        
+        // method to compare indexes
+
+        static void Task(string num1, string num2)
+
+        {
+            int a = num1[0] + num2[0];
+            int b = num1[1] + num2[1];
+            int c = num1[2] + num2[2];
+
+                    
+            if (a == b && b == c)
+
             {
                 Console.WriteLine();
-                Console.WriteLine("Not a " + digits + " digit number.");
-                goto First;
-            }                                 
-                             
-            // Second Input 
+                Console.WriteLine("True");
+                Console.WriteLine();
+            }
 
-            Second:
-            Console.WriteLine();
-            Console.WriteLine("Enter another " + digits + " digit number.");
-            Console.WriteLine();
-            string num2 = Console.ReadLine();
-            int len2 = num2.Length;
+            else
 
-            // Input Validation
-
-            if (len2 != digitsCount)
             {
                 Console.WriteLine();
-                Console.WriteLine("Not " + digits + " digits.");
-                goto Second;
-            }                                   
-                                                             
-                // Assigning variables to indexes                                           
+                Console.WriteLine("False");
+                Console.WriteLine();
+            }
 
-                int a = num1[0] + num2[0];
-                int b = num1[1] + num2[1];
-                int c = num1[2] + num2[2];
-                int d = num1[3] + num2[3];
-                /*int e = num1[4] + num2[4];
-                int f = num1[5] + num2[5];
-                int g = num1[6] + num2[6];
-                int h = num1[7] + num2[7];
-                int i = num1[8] + num2[8];
-                int j = num1[9] + num2[9];*/          
-                                                          
-                       // Comparing indexes between num1 and num2
 
-                       if (a == b && b == c && c == d) /*&& d == e && e == f &&
-                           f == g && g == h && h == i && i == j)*/
-                         
-                       {
-                       Console.WriteLine();                
-                       Console.WriteLine("True");
-                       }
-
-                       else
-
-                       {
-                       Console.WriteLine();
-                       Console.WriteLine("False");
-                       }
-
-                        Console.WriteLine();
-
-                                            /*//get and display the indices and values of first number
-
-                                            foreach (var item1 in num1.Select((value, index) => new { Value = value, Index = index }))
-                                            {
-
-                                                int num1Value = item1.Value;
-                                                int num1Index = item1.Index;
-                                                {
-                                                    Console.WriteLine();
-                                                    Console.WriteLine(item1);
-                                                }
-                                            }
-
-                                            //get and display the indices and value of second number
-
-                                            foreach (var item2 in num2.Select((value, index) => new { Value = value, Index = index }))
-                                                        {
-                                                            int num2Value = item2.Value;
-                                                            int num2Index = item2.Index;
-                                                            {
-                                                                Console.WriteLine();
-                                                                Console.WriteLine(item2);
-                                                            }
-                                                        }
-                                             */
-
-                        
-                        Console.WriteLine();
-                        Console.WriteLine("Press any key to try again.");
-                        Console.ReadKey(true);
-                        Console.WriteLine();
-                        goto First;
-
-        }       
+        }
     }
 }
                                                
