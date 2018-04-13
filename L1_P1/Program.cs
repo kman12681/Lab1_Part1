971137aa824b951bec2d66fea1ccbb1c2430dc07
kman12681 <keith.rigling@gmail.com>
2018-04-12 20:06:01 -0400

    Lab1_Part1_Submission

--- L1_P1/Program.cs
+++ L1_P1/Program.cs
@@ -7,21 +7,21 @@ namespace L1_P1
         static void Main(string[] args)
 
         {
-                // Welcome User
+          // Welcome User
 
-                Console.WriteLine();
-                Console.WriteLine("Hello, this program will assess whether the individual digits in two numbers all add to the same value.");
-                Console.WriteLine();
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadKey(true);
+          Console.WriteLine();
+          Console.WriteLine("Hello, this program will assess whether the individual digits in two numbers all add to the same value.");
+          Console.WriteLine();
+          Console.WriteLine("Press any key to continue.");
+          Console.ReadKey(true);
 
-                Console.WriteLine();
-                Console.WriteLine("The console will read 'True' if the digits add to the same value, and 'False' if they do not.");
-                Console.WriteLine();
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadKey(true);
+          Console.WriteLine();
+          Console.WriteLine("The console will read 'True' if the digits add to the same value, and 'False' if they do not.");
+          Console.WriteLine();
+          Console.WriteLine("Press any key to continue.");
+          Console.ReadKey(true);
 
-                // First Input
+          // First Input
 
                 while (true)
 
@@ -29,7 +29,8 @@ namespace L1_P1
 
                 string num1;
 
-                while (true) // program won't continue until three digits entered
+                while (true) 
+
                     {
                         Console.WriteLine();
                         Console.WriteLine("Please enter a three digit number:");
@@ -38,11 +39,13 @@ namespace L1_P1
                         int firstNumber;
                         bool num1Bool = Int32.TryParse(num1, out firstNumber);
 
-                        // Input Validation
+                        // Input Validation, won't continue until 3 digits and numbers entered
 
                         if (num1.Length == 3 && num1Bool)
                             break;
+
                         else
+
                         {
                             Console.WriteLine();
                             Console.WriteLine("Not a three digit number.");
@@ -53,7 +56,7 @@ namespace L1_P1
 
                     string num2;
 
-                    while (true) // won't continue until three digits entered
+                    while (true) 
                     {
                         Console.WriteLine();
                         Console.WriteLine("Enter another three digit number.");
@@ -74,32 +77,30 @@ namespace L1_P1
                         }
                     }
                     
-                    // calling Task method
+                        // calling Task method
 
-                    Task(num1, num2);
-
-
-                    string response = DoAgain();
-
-                if (response == "N")
-                    break;
-                                        
-                                                   
-                   
+                        Task(num1, num2);
+                
+                        string response = DoAgain();
+                            if (response == "N")
+                                break;                  
                 }
 
             }
 
+        // Method to run program again (or exit)
+
         static string DoAgain()
 
         {
-            while (true) // will run program again if user enters "Y"
+            while (true) // will run program again if user enters "Y" (or "y"), will exit if "N" (or "n")
 
             {
 
                 Console.WriteLine("Do you want to try again? (Y or N)");
+                Console.WriteLine();
                 string input = Console.ReadLine().ToUpper();
-
+                
                 if (input == "Y" || input == "N")
                     return input;
 
@@ -114,17 +115,17 @@ namespace L1_P1
             }
         }
 
-        // method to compare indexes
+        // Method to compare indexes
 
         static void Task(string num1, string num2)
 
         {
-            int a = num1[0] + num2[0]; // compares hundreds values
-            int b = num1[1] + num2[1]; // compares tens values
-            int c = num1[2] + num2[2]; // compares ones values
+            int a = num1[0] + num2[0]; // add hundreds values, stores in variable
+            int b = num1[1] + num2[1]; // add tens values, stores in variable
+            int c = num1[2] + num2[2]; // add ones values, stores in variable
 
                     
-            if (a == b && b == c)
+            if (a == b && b == c)   // check if variables are the same
 
             {
                 Console.WriteLine();
